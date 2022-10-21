055617a65a647d28877849d3e5b372d162c8c9c1
Triyono <yono.tri@gmail.com>
2022-10-21 14:58:32 +0700

    remove env

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_marshmallow import Marshmallow
 app = Flask(__name__)
 app.app_context().push()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://useranme_anda:password_anda@localhost/flaskmysql'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/flaskmysql'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
